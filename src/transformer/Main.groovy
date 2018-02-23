03a88a8dd99ee1c25aeb7fc622039ec0e723446a
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-23 17:00:23 +0000

    Passes in columns to RecordGenerator

--- src/transformer/Main.groovy
+++ src/transformer/Main.groovy
@@ -2,7 +2,14 @@ package transformer
 
 def resourcesDirectory = './../../resources/'
 
-def textGenerator = new TextGenerator("${ resourcesDirectory }Data1.txt")
+def columns = new ArrayList()
+columns.add(new Column('Apple', 6, 1))
+columns.add(new Column('Banana', 2, 2))
+columns.add(new Column('Carrot', 5, 2))
+
+def recordGenerator = new RecordGenerator(columns)
+
+def textGenerator = new TextGenerator("${ resourcesDirectory }Data1.txt", recordGenerator)
 textGenerator.generateFile()
 
 def input = new FileReader("${ resourcesDirectory }lines.txt")
