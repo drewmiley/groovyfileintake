5ad1071d3ae26c4f3e923819f1828771c347b3aa
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-26 17:37:24 +0000

    Adds empty unit tests for complex file comparison

--- test/transformer/FileComparatorTest.groovy
+++ test/transformer/FileComparatorTest.groovy
@@ -2,6 +2,12 @@ package transformer
 
 class FileComparatorTest extends GroovyTestCase {
 
+    private FileComparator simpleFileComparator
+
+    private List<String> simpleCompare1File
+
+    private List<String> simpleCompare2File
+
     private FileComparator fileComparator
 
     private List<String> compare1File
@@ -10,18 +16,32 @@ class FileComparatorTest extends GroovyTestCase {
 
     void setUp() {
         FileReader fileReader = new FileReader('resources/compare1.txt')
-        compare1File = fileReader.getFileData()
+        simpleCompare1File = fileReader.getFileData()
 
         fileReader = new FileReader('resources/compare2.txt')
+        simpleCompare2File = fileReader.getFileData()
+
+        simpleFileComparator = new FileComparator(simpleCompare1File, simpleCompare2File)
+
+        String matchColumnName = 'id'
+        List<Column> columns = new ArrayList()
+        columns.add(new Column(matchColumnName, 6, 1))
+        columns.add(new Column('Banana', 2, 2))
+        columns.add(new Column('Carrot', 5, 2))
+
+        fileReader = new FileReader('resources/record1.txt')
+        compare1File = fileReader.getFileData()
+
+        fileReader = new FileReader('resources/record2.txt')
         compare2File = fileReader.getFileData()
 
-        fileComparator = new FileComparator(compare1File, compare2File)
+        fileComparator = new FileComparator(compare1File, compare2File, matchColumnName, columns)
     }
 
     void tearDown() {
-        fileComparator = null
-        compare1File = null
-        compare2File = null
+        simpleFileComparator = null
+        simpleCompare1File = null
+        simpleCompare2File = null
     }
 
     void testRowsCommon() {
@@ -33,18 +53,30 @@ class FileComparatorTest extends GroovyTestCase {
         expected.add('compare5')
         expected.add('compare6')
         expected.add('compare7')
-        assert fileComparator.common() == expected
+        assert simpleFileComparator.common() == expected
     }
 
     void testRowsDeltaLeft() {
         List<String> expected = new ArrayList<String>()
         expected.add('fdsgsgsdgdsgdg')
-        assert fileComparator.deltaLeft() == expected
+        assert simpleFileComparator.deltaLeft() == expected
     }
 
     void testRowsDeltaRight() {
         List<String> expected = new ArrayList<String>()
         expected.add('wewerwrewe')
-        assert fileComparator.deltaRight() == expected
+        assert simpleFileComparator.deltaRight() == expected
+    }
+
+    void testRowsCommonWithColumnNames() {
+
+    }
+
+    void testRowsDeltaLeftWithColumnNames() {
+
+    }
+
+    void testRowsDeltaRightWithColumnNames() {
+
     }
 }
