afadfdc66778bae03da483a98dafaa15be76f222
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-27 11:58:22 +0000

    Adds unit tests

--- test/transformer/FileComparatorTest.groovy
+++ test/transformer/FileComparatorTest.groovy
@@ -69,14 +69,32 @@ class FileComparatorTest extends GroovyTestCase {
     }
 
     void testRowsCommonWithColumnNames() {
-
+        List<String> expected = new ArrayList<String>()
+        expected.add('1Some  1S  1Some')
+        expected.add('2Some  2S  2Dumb')
+        expected.add('3Some  3S  3Some')
+        expected.add('5Some  5S  5Some')
+        expected.add('6Some  6S  6Dumb')
+        expected.add('7Some  7S  7Dumb')
+        expected.add('10Some 10  10Som')
+        assert fileComparator.common() == expected
     }
 
     void testRowsDeltaLeftWithColumnNames() {
-
+        List<String> expected = new ArrayList<String>()
+        expected.add('0Some  0S  0dfds')
+        expected.add('4Dumb  4S  4wrew')
+        expected.add('8Some  8S  8Some')
+        expected.add('9Some  9S  9Dumb')
+        assert fileComparator.deltaLeft() == expected
     }
 
     void testRowsDeltaRightWithColumnNames() {
-
+        List<String> expected = new ArrayList<String>()
+        expected.add('0Some  0S  0Some')
+        expected.add('4Dumb  4S  4Some')
+        expected.add('8Some  8S  8wert')
+        expected.add('9Somd  9S  9Dumb')
+        assert fileComparator.deltaRight() == expected
     }
 }
